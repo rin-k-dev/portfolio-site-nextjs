2234aec656d80d0895522d23e01d39b0921d55bb
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-01-28 19:15:05 +0900

    works added

--- components/works.js
+++ components/works.js
@@ -1,11 +1,31 @@
 import styles from 'styles/works.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
-export default function Works() {
+export default function Works({ posts }) {
     return (
         <section className={styles.pSectionWorks}>
             <h2>Works</h2>
             <h3>制作実績</h3>
+            <div className={styles.pSectioinWorksItems}>
+                {posts.map(({ title, slug, eyecatch }) => (
+                    <article className={`${styles.post}`} key={slug}>
+                        <Link href={`/works/${slug}`}>
+                            <figure>
+                                <Image
+                                    src={eyecatch.url}
+                                    alt=""
+                                    width={500}
+                                    height={300}
+                                    sizes="(min-width: 768px) 500px, 84%"
+                                    placeholder="blur"
+                                    blurDataURL={eyecatch.blurDataURL}
+                                />
+                            </figure>
+                        </Link>
+                    </article>
+                ))}
+            </div>
             <div className={styles.cMoreButton}>
                 <Link href="/works">
                     もっと見る
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import MainVisual from 'components/main-visual'
 import Description from 'components/description'
 import Service from 'components/service'
 import Works from 'components/works'
+import { getPlaiceholder } from 'plaiceholder'
 
-export default function Home() {
+import { eyecatchLocal } from 'lib/constants'
+
+export default function Home({ posts }) {
     return (
         <>
             <Container large>
@@ -13,10 +17,26 @@ export default function Home() {
                 <MainVisual />
                 <Description />
                 <Service />
-                <Works />
+                <Works posts={posts}  />
             </Container> 
         </>
-        
-        
     )
+}
+
+export async function getStaticProps() {
+    const posts = await getAllPosts(2)
+
+    for (const post of posts) {
+        if (!post.hasOwnProperty('eyecatch')) {
+            post.eyecatch = eyecatchLocal
+        }
+        const { base64 } = await getPlaiceholder(post.eyecatch.url)
+        post.eyecatch.blurDataURL = base64
+    }
+
+    return {
+        props: {
+            posts: posts,
+        },
+    }
 }
