ef2fb97966dafa63e41b4f7aa77a0607618bcb50
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-02-01 21:52:23 +0900

    update

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,41 @@
-export default function Hero({ title, subtitle, imageOn = false }) {
+import styles from 'styles/hero.module.css'
+import Image from 'next/image'
+import worksbgsp from 'images/works-bg-sp.png'
+import worksbgpc from 'images/works-bg-pc.png'
+
+export default function Hero({ title, subtitle }) {
     return (
-        <div>
-            <h1>{title}</h1>
-            <p>{subtitle}</p>
-            {imageOn && <figure>[Picture]</figure>}
+        <div className={styles.lHeroContainer}>
+            <div>
+                <h1>{title}</h1>
+                <h2>{subtitle}</h2>
+            </div>
+            <Image className={styles.uDisplaySpOnly}
+                src={worksbgsp}
+                alt=""
+                width={1440}
+                height={350}
+                sizes="100vw"
+                priority
+                blur
+                style={{
+                    width: '100%',
+                    height: 'auto',
+                }}
+            />
+            <Image className={styles.uDisplayPcOnly}
+                src={worksbgpc}
+                alt=""
+                width={1440}
+                height={350}
+                sizes="100vw"
+                priority
+                blur
+                style={{
+                    width: '100%',
+                    height: 'auto',
+                }}
+            />
         </div>
     )
 }
--- pages/works/index.js
+++ pages/works/index.js
@@ -4,7 +4,7 @@ import Hero from 'components/hero'
 export default function Work() {
     return (
         <>
-            <Meta pageTitle="作品" pageDesc="作品一覧" />
+            <Meta pageTitle="作品" pageDesc="作品一覧"/>
             <Hero
                 title="Works"
                 subtitle="制作実績"
