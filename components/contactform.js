f483c47b3e5d0d7bcd2cd7309bf89ab18b3f68a5
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-02-01 18:32:56 +0900

    update

--- components/contactform.js
+++ components/contactform.js
@@ -12,37 +12,45 @@ export default function ContactForm() {
     }
     return (
         <section className={styles.pSectionContact}>
-            <h2>Contact</h2>
-            <h3>お問い合わせ</h3>
-            <p>
-                Web制作のお仕事のご依頼・その他のご相談などございましたら、お気軽に下記フォームよりお問い合わせください。<br /><br />
-                <span className={styles.pSectionContactRequired}>*</span>は必須項目です。
-            </p>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name<span className={styles.pSectionContactRequired}>*</span></label>
-                    <span>
-                        <input type="text" name="name" size="40" autoComplete="name" required />
-                        <ValidationError prefix="Name" field="name" errors={state.errors} />
-                    </span>
-                    <label htmlFor="email">E-Mail<span className={styles.pSectionContactRequired}>*</span></label>
-                    <span>
-                        <input type="email" name="email" size="40" autoComplete="email" required></input>
-                        <ValidationError prefix="Email" field="email" errors={state.errors} />
-                    </span>
-                </div>
-                <div>
-                    <label htmlFor="message">Message<span className={styles.pSectionContactRequired}>*</span></label>
-                    <span>
-                        <textarea name="message" cols="41" rows="11" required></textarea>
-                        <ValidationError prefix="Message" field="message" errors={state.errors} />
-                    </span>
-                </div>
-                <button type="submit" disabled={state.submitting} className={styles.cSendButton}>
-                    送信
-                </button>
-                <ValidationError errors={state.errors} />
-            </form>
+            <div className={styles.pSectionCantactContainer}>
+                <h2>Contact</h2>
+                <h3>お問い合わせ</h3>
+                <p>
+                    Web制作のお仕事のご依頼・その他のご相談などございましたら、お気軽に下記フォームよりお問い合わせください。<br /><br />
+                    <span className={styles.pSectionContactRequired}>*</span>は必須項目です。
+                </p>
+
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.flexContainer}>
+                        <div>
+                            <label htmlFor="name">Name<span className={styles.pSectionContactRequired}>*</span></label>
+                            <span>
+                                <input type="text" name="name" size="40" autoComplete="name" required />
+                                <ValidationError prefix="Name" field="name" errors={state.errors} />
+                            </span>
+                        </div>
+                        <div>
+                            <label htmlFor="email">E-Mail<span className={styles.pSectionContactRequired}>*</span></label>
+                            <span>
+                                <input type="email" name="email" size="40" autoComplete="email" required></input>
+                                <ValidationError prefix="Email" field="email" errors={state.errors} />
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message<span className={styles.pSectionContactRequired}>*</span></label>
+                        <span>
+                            <textarea name="message" cols="41" rows="11" required></textarea>
+                            <ValidationError prefix="Message" field="message" errors={state.errors} />
+                        </span>
+                    </div>
+                    <button type="submit" disabled={state.submitting} className={styles.cSendButton}>
+                        送信
+                    </button>
+                    <ValidationError errors={state.errors} />
+                </form>
+            </div>
+            
         </section>
     ) 
 }
