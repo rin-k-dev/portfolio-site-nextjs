e60cc51594bc4071e7aa92e287925b566111a110
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-02-01 21:59:55 +0900

    update

--- pages/works/index.js
+++ pages/works/index.js
@@ -1,15 +1,16 @@
 import Meta from 'components/meta'
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Works() {
     return (
-        <>
+        <Container large>
             <Meta pageTitle="作品" pageDesc="作品一覧"/>
             <Hero
                 title="Works"
                 subtitle="制作実績"
             />
-        </>
+        </Container>
         
     )
 }
