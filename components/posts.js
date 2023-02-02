d2e91a5b715e77f2d784f2fe05f53084d0da6691
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-02-02 20:45:54 +0900

    update

---
+++ components/posts.js
@@ -0,0 +1,11 @@
+import styles from 'styles/posts.module.css'
+import Link from 'next/link'
+import Image from ' next/image'
+
+export default function Posts({ posts }) {
+    return (
+        <div className={styles.Container}>
+            
+        </div>
+    )
+}
