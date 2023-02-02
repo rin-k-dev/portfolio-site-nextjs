fd17fd894521783cdf4603b1049625908c750711
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-02-02 20:30:19 +0900

    update

--- pages/works/index.js
+++ pages/works/index.js
@@ -1,6 +1,11 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import { getPlaiceholder } from 'plaiceholder'
+
+//Eyecatch for local
+import { eyecatchLocal } from 'lib/constants'
 
 export default function Works() {
     return (
@@ -13,4 +18,22 @@ export default function Works() {
         </Container>
         
     )
+}
+
+export async function getStaticProps() {
+    const posts = await getAllPosts()
+
+    for (const post of posts) {
+        if (!post.hasOwnProperty('eyecatch')) {
+            post.eyecatch = eyecatchLocal
+        }
+        const { base64 } = await getPlaiceholder(post.eyecatch.url)
+        post.eyecatch.blurDataURL = base64
+    }
+
+    return {
+        props: {
+            posts: posts,
+        },
+    }
 }
