8fec3124627d5748b27085e809b2f8d173fc8978
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-02-03 14:31:59 +0900

    update

--- components/logo.js
+++ components/logo.js
@@ -3,6 +3,6 @@ import styles from 'styles/logo.module.css'
 
 export default function Logo() {
     return (
-        <Link href="/" className={styles.cLogo}>Rin&apos;s Portfolio</Link>
+        <Link href="/" className={styles.cLogo}>Rin’s Portfolio</Link>
     )
 }
