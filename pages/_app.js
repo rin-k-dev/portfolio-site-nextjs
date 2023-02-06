72b46a44f04d8475d34f0ebec922ca69b7fd56a6
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-02-06 16:36:03 +0900

    Google tag manager added

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import '@/styles/globals.css'
 import Layout from 'components/layout'
 import Script from 'next/script'
@@ -10,7 +12,24 @@ config.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
     return (
-        <>  
+        <>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+                }}
+            />
             <Layout>
                 <Component {...pageProps} />
             </Layout>
