a4b278edb21066cda943da0b633ac456cf6ad2a3
rink <77056883+rin-k-dev@users.noreply.github.com>
2023-02-06 23:23:12 +0900

    footer changed

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import styles from 'styles/footer.module.css'
 
 export default function Footer() {
-    return <footer className={styles.pFooter}><small>© Rin K All Rights Reserved</small></footer>
+    return <footer className={styles.pFooter}><small>© Rin K All Rights Reserved.</small></footer>
 }
